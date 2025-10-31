8c04ea8b725add52fac2dbbe4455b6de9a2fec82
yerman2 <yermanratia2@gmail.com>
2025-10-31 16:53:10 -0400

    Initial Commit

---
+++ Kakebo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kakebo
+//
+//  Created by German Rattia on 31/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kakebo/KakeboApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakeboApp.swift
+//  Kakebo
+//
+//  Created by German Rattia on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KakeboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
